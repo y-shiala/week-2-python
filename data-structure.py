0e247b38b814631de133009b349b8fc893f97119
Yvonnah Shiala <104901503+y-shiala@users.noreply.github.com>
2025-04-23 08:41:59 +0300

    week-2 python assignment submission

---
+++ data-structure.py
@@ -0,0 +1,26 @@
+#creating a list
+my_list = []
+
+#appending 10, 20, 30, 40,
+
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+#inserting 15 to the list
+
+my_list.insert(1, 15)
+
+#extending my_list
+
+my_list.extend([50, 60, 70])
+
+#removing last element
+my_list.pop()
+
+#sorting my_list
+my_list.sort()
+
+#index of value 30
+print(my_list.index(30))
